899012db475ceb2324e93b6bcb2c8fa1dfbdcf62
Michael Rodrigues <run.michael97@gmail.com>
2024-07-08 01:07:58 -0300

    chore: remove spaces and turn all string in lowercase

--- is-this-palindrome/main.kt
+++ is-this-palindrome/main.kt
@@ -2,23 +2,22 @@ fun main(args: Array<String>) {
 
     if (args.isNotEmpty() && args.size == 1) {
 
-   	var str = args[0]
+   	var str = args[0].replace("\\s+".toRegex(), "").lowercase()
 
 	var leftPointer = 0
 	var rightPointer = str.length - 1
-
-
+	
 	while (leftPointer < rightPointer) {
-
+		
 		if (str[leftPointer] != str[rightPointer]) {
-			println("$str não é um palíndromo mano")
+			println("${args[0]} não é um palíndromo mano")
 			return	
 		}
 		leftPointer++
 		rightPointer--
 
 	}
-	println("$str é um palíndromo!")
+	println("${args[0]} é um palíndromo!")
 //        println("leftPointer: $leftPointer, rightPointer: $rightPointer")
     } else {
 	println("Deve ser passado apenas o nome do programa e um parametro entre aspas! Como o exemplo abaixo!")
